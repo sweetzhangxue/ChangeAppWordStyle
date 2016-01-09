bc64199c6f7a5b274b5c3a85d90885507511f374
XueZhang <xue.zhang@aayongche.com>
2016-01-09 11:04:21 +0800

    ..

--- app/src/main/java/com/example/sweet_xue/changeappwordstyle/MainActivity.java
+++ app/src/main/java/com/example/sweet_xue/changeappwordstyle/MainActivity.java
@@ -14,9 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv  = (TextView) findViewById(R.id.tv);
-        Typeface typeface = Typeface.createFromAsset(getResources().getAssets(),"SourceHanSansCN-Regular.ttf");
-        tv.setTypeface(typeface);
     }
 
     @Override
